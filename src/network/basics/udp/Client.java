4c3777a1f1cadbda281f769a3e6da192552c6ccd
xoxolische <nikitos_ua@ukr.net>
2018-03-30 13:56:10 +0300

    add draft version of udp version

--- src/network/basics/udp/Client.java
+++ src/network/basics/udp/Client.java
@@ -1,5 +1,25 @@
 package network.basics.udp;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Client {
 
+	public static void main(String[] args) {
+		try {
+			int port = 2453;
+
+			InetAddress address = InetAddress.getByName("127.0.0.1");
+			DatagramSocket socket = new DatagramSocket();
+
+			byte[] m = "hello from a client".getBytes();
+
+			DatagramPacket request = new DatagramPacket(m, m.length, address, port);
+			socket.send(request);
+			socket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/network/basics/udp/Server.java
+++ src/network/basics/udp/Server.java
@@ -1,5 +1,36 @@
 package network.basics.udp;
 
-public class Server {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
 
+public class Server {
+	public static void main(String[] args) {
+		int port = 2453;
+		try {
+			DatagramSocket ds = new DatagramSocket(port);
+
+			System.out.println("UDP Waiting for a packets...");		
+			
+			
+			byte[] buf = new byte[256];
+			DatagramPacket request = new DatagramPacket(buf, buf.length);
+			ds.receive(request);
+			System.out.println(new String(request.getData(), StandardCharsets.UTF_8));
+			
+			ds.close();
+//
+//			InetAddress clientAddress = request.getAddress();
+//			int clientPort = request.getPort();
+//			 
+//			String data = "Message from server";
+//			buf = data.getBytes();
+//			 
+//			DatagramPacket response = new DatagramPacket(buf, buf.length, clientAddress, clientPort);
+//			ds.send(response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
