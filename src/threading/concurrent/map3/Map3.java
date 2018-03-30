5eba9bf3aa7859b68d856a0005dd8acc4724a511
xoxolische <nikitos_ua@ukr.net>
2018-03-30 16:39:04 +0300

    add fix with concurrentHashMap

--- src/threading/concurrent/map3/Map3.java
+++ src/threading/concurrent/map3/Map3.java
@@ -1,10 +1,80 @@
 package threading.concurrent.map3;
 
-public class Map3 {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Map3 extends Thread {
+
+	private static String[] data1;
+
+	private static Map<Integer, String> map = new ConcurrentHashMap<>();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		data1 = getWords();
 
+		Map3 m1 = new Map3();
+		m1.start();
+		Map3 m2 = new Map3();
+		m2.start();
+
+		try {
+			m1.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		try {
+			m2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Map<Integer, String> clean = new HashMap<>();
+		for (int i = 0; i < data1.length; i++) {
+			clean.put(data1[i].hashCode(), data1[i]);
+		}
+		System.out.println("??? : " + map.size());
+		System.out.println("Constant value : " + clean.size());
+
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < data1.length; i++) {
+			map.put(data1[i].hashCode(), data1[i] + this.getName());
+		}
+	}
+
+	private static BufferedReader getFileInBuffer(String fileName) {
+		try {
+			File f = new File("hp.txt");
+			FileReader fr = new FileReader(f);
+			BufferedReader br = new BufferedReader(fr);
+			return br;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private static String[] getWords() {
+		try {
+			List<String> words = new LinkedList<>();
+			BufferedReader br = getFileInBuffer("hp.txt");
+			String s = br.readLine();
+			while (s != null) {
+				words.add(s);
+				s = br.readLine();
+			}
+			return words.toArray(new String[words.size()]);
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 }
