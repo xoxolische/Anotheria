214d70750f4acf7e224983c3b0e101aea127caef
xoxolische <nikitos_ua@ukr.net>
2018-03-30 16:22:18 +0300

    add synchronized map solve

--- src/threading/concurrent/map2/Map2synchronized.java
+++ src/threading/concurrent/map2/Map2synchronized.java
@@ -1,4 +1,4 @@
-package threading.concurrent;
+package threading.concurrent.map2;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -8,37 +8,28 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import utils.BrokenString;
-
-public class Map2 extends Thread {
-
-	// 466544 lines
+public class Map2synchronized extends Thread {
 
 	private static String[] data1;
-
-	private static BrokenString[] data2;
 
 	private static Map<Integer, String> map = new HashMap<>();
 
 	public static void main(String[] args) {
 		data1 = getWords();
-		// data2 = getBrokenWords();
 
-		Map2 m1 = new Map2();
+		Map2synchronized m1 = new Map2synchronized();
 		m1.start();
-		Map2 m2 = new Map2();
+		Map2synchronized m2 = new Map2synchronized();
 		m2.start();
 
 		try {
 			m1.join();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		try {
 			m2.join();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -46,7 +37,6 @@ public class Map2 extends Thread {
 		for (int i = 0; i < data1.length; i++) {
 			clean.put(data1[i].hashCode(), data1[i]);
 		}
-		// System.out.println(map);
 		System.out.println("??? : " + map.size());
 		System.out.println("Constant value : " + clean.size());
 
@@ -55,10 +45,7 @@ public class Map2 extends Thread {
 	@Override
 	public void run() {
 		for (int i = 0; i < data1.length; i++) {
-			// System.out.println(data1[i] + " Hello from thread : " + this.getName());
 			sPut(data1[i].hashCode(), data1[i] + this.getName());
-
-			// map.put(data2[i].hashCode(), data2[i] + this.getName());
 		}
 	}
 
@@ -80,24 +67,15 @@ public class Map2 extends Thread {
 
 	private static String[] getWords() {
 		try {
-			// int c = 0;
 			List<String> words = new LinkedList<>();
 			BufferedReader br = getFileInBuffer("hp.txt");
 			String s = br.readLine();
 			while (s != null) {
-				// c++;
-				// String[] w = s.split(" ");
-				// for(String sw : w) {
-				// words.add(sw);
-				// }
 				words.add(s);
 				s = br.readLine();
 			}
-			// System.out.println(c);
-			// System.exit(0);
 			return words.toArray(new String[words.size()]);
 		} catch (Exception e) {
-			// TODO: handle exception
 			return null;
 		}
 	}
