0c2333ef375a65a503d77cdf08cc443d4a04c402
xoxolische <nikitos_ua@ukr.net>
2018-05-03 16:17:47 +0300

    to 8

--- KnightsTour/src/knight/single/tour/Main.java
+++ KnightsTour/src/knight/single/tour/Main.java
@@ -6,7 +6,7 @@ import knight.Knight;
 public class Main {
 
 	public static void main(String[] args) {
-		Knight k = new Knight(5, 3);
+		Knight k = new Knight(0, 0);
 		Board b = new Board(8);
 		SingleSolutionFinder s = new SingleSolutionFinder(k, b);
 		s.search();
